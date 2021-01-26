5d6a41f9419ee700c2129520f1b3a58da2f61f97
matmang <64837797+matmang@users.noreply.github.com>
2021-01-26 16:40:37 +0900

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import corona_alrimi.today_corona as today
+import corona_alrimi.corona_etc as etc
+
+today.today_dead()
+today.today_confirm_total()
+
+etc.corona_by_age(35)
+etc.corona_by_gender()
