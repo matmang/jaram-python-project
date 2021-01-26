6b8ecf098aa415a65746b3de8cb4396903e0cbb7
matmang <64837797+matmang@users.noreply.github.com>
2021-01-26 16:55:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import corona_alrimi.today_corona as today
 import corona_alrimi.corona_etc as etc
+import corona_alrimi.overseas as ovs
 
 today.today_dead()
 today.today_confirm_total()
 
 etc.corona_by_age(35)
 etc.corona_by_gender()
+
+ovs.overseas_asia()
+ovs.overseas_china()
