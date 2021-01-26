5c1663882c16d59d2962bc950877a8677b1be9ab
matmang <64837797+matmang@users.noreply.github.com>
2021-01-26 13:49:41 +0900

    Update today_corona.py

--- corona_alrimi/today_corona.py
+++ corona_alrimi/today_corona.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import re
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,7 +8,7 @@ from bs4 import BeautifulSoup
 import time
 
 
-def today_confirm():
+def today_confirmTotal():
     driver = webdriver.Chrome(executable_path=r'.\chromedriver.exe')
     url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
     driver.get(url)
@@ -21,6 +20,36 @@ def today_confirm():
     return confirm_num.get_text()
 
 
+def today_confirmNational():
+    driver = webdriver.Chrome(executable_path=r'.\chromedriver.exe')
+    url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
+    driver.get(url)
+
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    confirm_num = soup.select('p[class*="_"]')[2]
+    res = ''
+    for i in confirm_num :
+        res += i
+    return res
+
+
+def today_confirmOversea():
+    driver = webdriver.Chrome(executable_path=r'.\chromedriver.exe')
+    url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
+    driver.get(url)
+
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    confirm_num = soup.select('p[class*="_"]')[3]
+    res = ''
+    for i in confirm_num :
+        res += i
+    return res
+
+
 def today_release():
     driver = webdriver.Chrome(executable_path=r'.\chromedriver.exe')
     url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
@@ -29,8 +58,40 @@ def today_release():
     src = driver.page_source
     soup = BeautifulSoup(src, "html.parser")
 
-    checkup_num = soup.select_one('span.txt_ntc')
-    return checkup_num.get_text()
+    checkup_num = soup.select('span[class*="_"]')[1]
+    res = ''
+    for i in checkup_num:
+        res += i
+    return res
+
+def today_quarantine():
+    driver = webdriver.Chrome(executable_path=r'.\chromedriver.exe')
+    url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
+    driver.get(url)
+
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    quarantine = soup.select('span[class*="_"]')[2]
+    res = ''
+    for i in quarantine :
+        res += i
+    return res
 
 
-print(today_release())
+def today_dead():
+    driver = webdriver.Chrome(executable_path=r'.\chromedriver.exe')
+    url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
+    driver.get(url)
+
+    src = driver.page_source
+    soup = BeautifulSoup(src, "html.parser")
+
+    dead = soup.select('span[class*="_"]')[3]
+    res = ''
+    for i in dead :
+        res += i
+    return res
+
+
+print(today_confirmOversea())
