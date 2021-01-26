78266952bc6f7a77867de7404ed437f17a0a80b1
matmang <64837797+matmang@users.noreply.github.com>
2021-01-26 11:35:53 +0900

    Update today_corona.py

--- corona_alrimi/today_corona.py
+++ corona_alrimi/today_corona.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,7 +8,7 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
-driver = webdriver.Chrome(executable_path = r'C:\Users\jysko\python programming\chromedriver\chromedriver.exe')
+driver = webdriver.Chrome(executable_path = r'.\chromedriver.exe')
 url = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%BD%94%EB%A1%9C%EB%82%98+%ED%98%84%ED%99%A9&oquery=%EC%BD%94%EB%A1%9C%EB%82%98+%ED%98%84%ED%99%A9&tqi=hs9wBwprvOsssMu%2BeOVssssstmh-256212'
 driver.get(url)
 
